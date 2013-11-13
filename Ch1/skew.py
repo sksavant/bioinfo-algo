6eec6a0fbde53a4455c2792a65d688dcc89715a2
Savant Krishna <savant.2020@gmail.com>
2013-11-14 03:08:21 +0530

    skew finding

--- Ch1/skew.py
+++ Ch1/skew.py
@@ -6,18 +6,27 @@ genome = raw_input()
 
 l = len(genome)
 
-skewlist = [0 for i in range(l+1)]
+#skewlist = [0 for i in range(l+1)]
 
 diffgc = 0
 
-minskews = [0]
+minskew = 0
+#nmin = 1
+minlist = [0]
 
 for i in range(l):
     if genome[i]=='G':
         diffgc=diffgc+1
     elif genome[i]=='C':
         diffgc=diffgc-1
-    if 
-    skewlist[i+1]=diffgc
+    if diffgc<minskew:
+        #nmin=1
+        minlist = [i+1]
+        minskew = diffgc
+    elif diffgc==minskew:
+        #nmin=nmin+1
+        minlist.append(i+1)
+    #skewlist[i+1]=diffgc
 
-print " ".join(map(str,skewlist))
+#print " ".join(map(str,skewlist))
+print " ".join(map(str,minlist))
