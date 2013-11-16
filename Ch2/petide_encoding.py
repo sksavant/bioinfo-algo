ed888230800d5bc6e2705ddd2156e3b5cdf834c1
Savant Krishna <savant.2020@gmail.com>
2013-11-16 22:25:51 +0530

    no sequence in bacteria

--- Ch2/petide_encoding.py
+++ Ch2/petide_encoding.py
@@ -12,12 +12,20 @@ def find_possible_encoders_helper(dna_text, peptide):
         subs = text[i:i+sublen]
         if rna_to_amino.translate_rna(subs)==peptide:
             substrings.append(dna_text[i:i+sublen])
+    print "Done with one dna seq"
     return substrings
 
 def find_possible_encoders(text, peptide):
     return find_possible_encoders_helper(text,peptide)+map(revcomp.get_complement,find_possible_encoders_helper(revcomp.get_complement(text),peptide))
 
 if __name__=="__main__":
+    bacteria = open("B_brevis.txt","r")
+    b = "".join(bacteria.read().split('\n'))
+    peptide = "VKLFPWFNQY"
+    #print b
+    print "\n".join(find_possible_encoders(b,peptide)),
+    '''
     text = raw_input()
     peptide = raw_input()
     print "\n".join(find_possible_encoders(text,peptide)),
+    '''
